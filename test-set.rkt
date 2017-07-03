2a4f7d390d1be3538411a001c7f7c1a156303f4d
XZentus <z_xmn@mail.ru>
2017-07-03 17:36:40 +0300

    Add files via upload

---
+++ test-set.rkt
@@ -0,0 +1,40 @@
+#lang racket
+
+(define (test-set a b c)
+  (define result 0)
+  (for ((x (in-range a b c)))
+    (set! result (+ result x)))
+  result)
+
+(define (test-set-do a b c)
+  (define result 0)
+  (do ((x a (+ x c)))
+    ((>= x b) result)
+    (set! result (+ result x))))
+
+(define (test-rec a b c)
+  (letrec
+      ((fun
+        (λ (x)
+          (if (>= x b)
+              0
+              (+ x (fun (+ x c)))))))
+    (fun a)))
+
+(define (test-rec-acc a b c)
+  (letrec
+      ((fun
+        (λ (x summ)
+          (if (>= x b)
+              summ
+              (fun (+ x c) (+ summ x))))))
+    (fun a 0)))
+
+(define (test-rec-acc-rev a b c)
+  (letrec
+      ((fun
+        (λ (x summ)
+          (if (< x b)
+              (fun (+ x c) (+ summ x))
+              summ))))
+    (fun a 0)))
