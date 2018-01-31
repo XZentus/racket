340a64009f09fe9c5158d68ef8dbc46504adef2e
XZentus <z_xmn@mail.ru>
2018-01-31 14:39:20 +0300

    Update x3tccomplex.rkt

--- x3tccomplex.rkt
+++ x3tccomplex.rkt
@@ -30,32 +30,31 @@
    (list
     (cons 'name name)
     (cons 'cycle-length cycle-length)
-    (cons 'wares
-          (for/list ((x factory-wares))
-            (apply make-factory-ware x))))))
+    (cons 'wares factory-wares))))
 
-(define (hms h m s) (+ (* h 60 60) (* m 60) s))
+(define (hms (h 0) (m 0) (s 0)) (+ (* h 60 60) (* m 60) s))
+(define-values (1h 1m) (values (hms 1) (hms 0 1)))
 
 (define factories-db
   (let ((h (make-hash)))
     (for ([x (list
-              (list "Пар Завод ракет \"Булава\" L" (hms 0 10 0) '(("Батареи" 'raw -750)
-                                                                  ("Руда" 'raw -125)
-                                                                  ("Соевая мука" 'raw -100)
-                                                                  ("Заградительная ракета \"Булава\"" 'final 5)))
-              (list "Пар Завод ракет \"Томагавк\" XL" (hms  0 20  0) '(("Батареи" 'raw -3000)
-                                                                       ("Руда" 'raw -500)
-                                                                       ("Соевая мука" 'raw -400)
-                                                                       ("Тяжелая ракета \"Томагавк\"" 'final 30)))
-              (list "Пар Соевая фабрика XL" (hms  0  1  0) '(("Батареи" 'raw -150)
-                                                             ("Соевые бобы" 'raw -120)
-                                                             ("Соевая мука" 'final 20)))
-              (list "Пар Соевая ферма XL" (hms 0 1 0) '(("Батареи" 'raw -150)
-                                                        ("Соевые бобы" 'final 120)))
+              (list "Пар Завод ракет \"Булава\" L" (hms 0 10) '(("Батареи" 'raw -750)
+                                                                ("Руда" 'raw -125)
+                                                                ("Соевая мука" 'raw -100)
+                                                                ("Заградительная ракета \"Булава\"" 'final 5)))
+              (list "Пар Завод ракет \"Томагавк\" XL" (hms 0 20) '(("Батареи" 'raw -3000)
+                                                                   ("Руда" 'raw -500)
+                                                                   ("Соевая мука" 'raw -400)
+                                                                   ("Тяжелая ракета \"Томагавк\"" 'final 30)))
+              (list "Пар Соевая фабрика XL" (hms 0 1) '(("Батареи" 'raw -150)
+                                                        ("Соевые бобы" 'raw -120)
+                                                        ("Соевая мука" 'final 20)))
+              (list "Пар Соевая ферма XL" (hms 0 1) '(("Батареи" 'raw -150)
+                                                      ("Соевые бобы" 'final 120)))
               (list "Рудная шахта XL 1-15-30" (hms 0 1 15) '(("Батареи" 'raw -180)
                                                              ("Руда" 'final 30)))
-              (list "Рудная шахта XL 1-3-30" (hms 0 1 15)  '(("Батареи" 'raw -180)
-                                                             ("Руда" 'final 30)))
+              (list "Рудная шахта XL 1-3-30" (hms 0 1 15) '(("Батареи" 'raw -180)
+                                                            ("Руда" 'final 30)))
               (list "Кремниевая шахта XL 1-58-20" (hms 0 1 58) '(("Батареи" 'raw -480)
                                                                  ("Кремниевые пластины" 'final 20))))])
       (hash-set! h (car x) (apply make-factory x)))
@@ -78,3 +77,36 @@
                  "Рудная шахта XL 1-15-30"
                  "Рудная шахта XL 1-3-30"
                  "Кремниевая шахта XL 1-58-20")))
+
+(define (union-using-type t1 t2)
+  (if (equal? t1 t2) t1 'inter))
+
+(define (calc-complex complex (factories-db factories-db))
+  (define wares-data (make-hash)) ; "name" -> (using-type . num)
+ 
+  (define (parse-factory factory-name multiplier)
+    (define factory (hash-ref factories-db factory-name))
+    (define factory-wares (hash-ref factory 'wares))
+    (define (calc-ware num mult)
+      (/ (* num mult) (hash-ref factory 'cycle-length)))
+    (for ((factory-ware factory-wares)) ; ( name using-type num)
+      (match factory-ware
+        [(list ware-name using-type num)
+         (define wd-elem (hash-ref wares-data ware-name #f)) ; (using-type . num)
+         (cond
+           [wd-elem
+            (hash-set! wares-data
+                       ware-name
+                       (cons (union-using-type (car wd-elem) using-type)
+                             (+ (calc-ware num multiplier) (cdr wd-elem))))]
+           [else
+            (hash-set! wares-data
+                       ware-name
+                       (cons using-type (calc-ware num multiplier)))])])))
+ 
+  (do ((id (hash-iterate-first complex) (hash-iterate-next complex id)))
+    ((eq? id #f))
+    (let ((f-num (hash-iterate-pair complex id)))
+      (parse-factory (car f-num) (cdr f-num))))
+  wares-data)
+        
